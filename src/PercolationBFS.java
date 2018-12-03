3680e286c5fb0b494636bd89ff0464d620f9010e
toby.chen@duke.edu <tobychen00@SamvedsiPhone2.wired.duke.local>
2018-12-02 20:29:34 -0500

    dfsfast and bfs

--- src/PercolationBFS.java
+++ src/PercolationBFS.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class PercolationBFS extends PercolationDFSFast{
 
@@ -6,6 +8,26 @@ public class PercolationBFS extends PercolationDFSFast{
 	}
 	@Override
 	protected void dfs(int row, int col) {
-		 
+		// out of bounds?
+		if (! inBounds(row,col)) {
+			throw new IndexOutOfBoundsException("this [row, col] is not in bounds");
+		}
+		int size = myGrid.length;
+		int[] rowDelta = {-1,1,0,0};
+		int[] colDelta = {0,0,-1,1};
+		Queue<Integer> qp = new LinkedList<>();       
+		myGrid[row][col] = FULL; 
+		qp.add(row*size + col);
+		while (qp.size() != 0){
+			Integer k = qp.remove();
+			for(int i=0; i < rowDelta.length; i++){
+				row = (k-col)/row + rowDelta[k];
+				col = (k-row*size) + colDelta[k];
+				if (inBounds(row,col) && myGrid[row][col] == OPEN){
+					qp.add(row*size + col);
+					myGrid[row][col] = FULL;
+				}
+			}
+		}
 	}
 }
--- src/PercolationDFSFast.java
+++ src/PercolationDFSFast.java
@@ -6,7 +6,7 @@ public class PercolationDFSFast extends PercolationDFS {
 	}
 	@Override
 	protected void updateOnOpen(int row, int col) {
-		if(isFull(row+1, col) || isFull(row-1, col+1) || isFull(row, col-1)) {
+		if(row == 0 || (inBounds(row+1, col) && isFull(row+1, col)) || (inBounds(row-1, col) && isFull(row-1, col)) || (inBounds(row, col-1) && isFull(row, col-1))|| (inBounds(row, col+1) && isFull(row, col+1))) {
 			dfs(row, col);
 		}
 	}
